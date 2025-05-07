ae38418dbff46bdf7d12c39bc29c848396e793a2
prasant46 <pdprasantapd46245@gmail.com>
2025-05-07 12:33:19 +0530

    Final correction done3

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ import cors from "cors";
 import mongoose from "mongoose";
 import UserRoutes from "./routes/User.js";
 
+
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
+
+
 
 app.use("/api/user/", UserRoutes);
 // error handler
